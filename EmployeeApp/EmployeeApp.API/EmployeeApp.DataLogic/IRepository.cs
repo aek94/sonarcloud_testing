f43a617a1f7fcb7ee6fe6114afd7b4bd169d720d
aek94 <krisnawanandrew@gmail.com>
2022-04-22 18:00:57 -0700

    add EmployeeApp to test sonarcloud

---
+++ EmployeeApp/EmployeeApp.API/EmployeeApp.DataLogic/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeApp.BusinessLogic;
+
+namespace EmployeeApp.DataLogic
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+        Task<IEnumerable<Employee>> GetEmployeeAsync(int Id);
+        Task<IEnumerable<Employee>> AddEmployeeAsync(Employee emp);
+        Task<IEnumerable<Employee>> UpdateEmployeeAsync(int Id, Employee emp);
+        Task<IEnumerable<Employee>> DeleteEmployeeAsync(int Id);
+
+        Task<IEnumerable<Location>> GetLocationsAsync();
+    }
+
+}
